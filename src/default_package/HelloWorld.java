7b67c457ac9ffedf5775a492ac328e4629e58acb
Ariel429 <arielkim429@gmail.com>
2020-09-27 15:24:32 +0900

    add message hi world

---
+++ src/default_package/HelloWorld.java
@@ -0,0 +1,10 @@
+package default_package;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+		System.out.println("Hi,World!");
+	}
+
+}
